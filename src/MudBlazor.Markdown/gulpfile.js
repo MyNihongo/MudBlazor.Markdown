44f4df2785359e421fe7d3a9ae1d43f97ca17d88
mihails.kuzmins <mihails.kuzmins@daytongroup.lv>
2021-07-17 22:51:29 +0300

    add a gulpfile

---
+++ src/MudBlazor.Markdown/gulpfile.js
@@ -0,0 +1,12 @@
+﻿const { src, dest } = require("gulp");
+const rename = require("gulp-rename");
+const minifyCss = require("gulp-minify-css");
+
+function css() {
+	return src("Resources/*css")
+		.pipe(minifyCss())
+		.pipe(rename({ extname: ".min.css" }))
+		.pipe(dest("wwwroot"));
+}
+
+exports.default = css
