55faa81d2e00fabab0fbaab73a259778a1fc6e0a
erri120 <erri120@protonmail.com>
2021-08-09 16:51:26 +0200

    Add AddIfNotNull extension

---
+++ tests/MudBlazor.Markdown.Tests/ComponentParameterCollectionBuilderEx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq.Expressions;
+using Bunit;
+using Microsoft.AspNetCore.Components;
+
+namespace MudBlazor.Markdown.Tests
+{
+    public static class ComponentParameterCollectionBuilderEx
+    {
+        public static ComponentParameterCollectionBuilder<TComponent> AddIfNotNull<TComponent, TValue>(
+            this ComponentParameterCollectionBuilder<TComponent> @this,
+            Expression<Func<TComponent, TValue>> parameterSelector,
+            TValue value)
+            where TComponent : IComponent
+        {
+            return value != null
+                ? @this.Add(parameterSelector, value)
+                : @this;
+        }
+    }
+}
--- tests/MudBlazor.Markdown.Tests/MudMarkdownTestsBase.cs
+++ tests/MudBlazor.Markdown.Tests/MudMarkdownTestsBase.cs
@@ -9,18 +9,17 @@ namespace MudBlazor.Markdown.Tests
 		private readonly TestContext _ctx = new();
 
 		protected IRenderedComponent<MudMarkdown> CreateFixture(string value, ICommand command = null, int? tableCellMinWidth = null,
-			Typo h1Typo = Typo.h1, Typo h2Typo = Typo.h2, Typo h3Typo = Typo.h3, Typo h4Typo = Typo.h4, Typo h5Typo = Typo.h5,
-			Typo h6Typo = Typo.h6) =>
+			Typo h1Typo = Typo.h1, Typo h2Typo = Typo.h2, Typo h3Typo = Typo.h3, Typo h4Typo = Typo.h4, Typo h5Typo = Typo.h5, Typo h6Typo = Typo.h6) =>
 			_ctx.RenderComponent<MudMarkdown>(@params =>
 				@params.Add(static x => x.Value, value)
 					.Add(static x => x.LinkCommand, command)
 					.Add(static x => x.TableCellMinWidth, tableCellMinWidth)
-					.Add(static x => x.H1Typo, h1Typo)
-					.Add(static x => x.H2Typo, h2Typo)
-					.Add(static x => x.H3Typo, h3Typo)
-					.Add(static x => x.H4Typo, h4Typo)
-					.Add(static x => x.H5Typo, h5Typo)
-					.Add(static x => x.H6Typo, h6Typo));
+					.AddIfNotNull(static x => x.H1Typo, h1Typo)
+					.AddIfNotNull(static x => x.H2Typo, h2Typo)
+					.AddIfNotNull(static x => x.H3Typo, h3Typo)
+					.AddIfNotNull(static x => x.H4Typo, h4Typo)
+					.AddIfNotNull(static x => x.H5Typo, h5Typo)
+					.AddIfNotNull(static x => x.H6Typo, h6Typo));
 
 		public void Dispose()
 		{
