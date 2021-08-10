a30199f642fcf3d2a4b8fea629c22b8fe8c3acb0
mihails.kuzmins <mihails.kuzmins@daytongroup.lv>
2021-08-10 19:07:59 +0900

    minify JS in gulp

---
+++ src/MudBlazor.Markdown/Resources/MudBlazor.Markdown.js
@@ -0,0 +1,3 @@
+﻿window.scrollToElementId = function(elementId) {
+
+}
--- src/MudBlazor.Markdown/gulpfile.js
+++ src/MudBlazor.Markdown/gulpfile.js
@@ -1,12 +1,19 @@
-﻿const { src, dest } = require("gulp");
+﻿const { src, dest, series } = require("gulp");
 const rename = require("gulp-rename");
 const minifyCss = require("gulp-minify-css");
+const minifyJs = require("gulp-minify");
 
 function css() {
-	return src("Resources/*css")
+	return src("Resources/*.css")
 		.pipe(minifyCss())
 		.pipe(rename({ extname: ".min.css" }))
 		.pipe(dest("wwwroot"));
 }
 
-exports.default = css
+function js() {
+	return src("Resources/*.js")
+		.pipe(minifyJs({ noSource: true, ext: { min: ".min.js" } }))
+		.pipe(dest("wwwroot"));
+}
+
+exports.default = series(css, js)
