8960aa12592784cd51f41b4861c627de0bdd97df
mihails.kuzmins <mihails.kuzmins@daytongroup.lv>
2021-09-12 22:19:33 +0300

    js with webpack

---
+++ src/MudBlazor.Markdown/webpack.config.js
@@ -0,0 +1,9 @@
+﻿const path = require("path");
+
+module.exports = {
+	entry: ["./Resources/Npm.js", "./Resources/MudBlazor.Markdown.js"],
+	output: {
+		path: path.resolve(__dirname, "wwwroot"),
+		filename: "MudBlazor.Markdown.min.js"
+	}
+};
