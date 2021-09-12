fa9a9610e870338b61b03538598580365084affc
mihails.kuzmins <mihails.kuzmins@daytongroup.lv>
2021-09-12 22:28:41 +0300

    css for webpack

--- src/MudBlazor.Markdown/webpack.config.js
+++ src/MudBlazor.Markdown/webpack.config.js
@@ -1,9 +1,31 @@
 ﻿const path = require("path");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-	entry: ["./Resources/Npm.js", "./Resources/MudBlazor.Markdown.js"],
+	entry: [
+		"./Resources/Npm.js",
+		 "./Resources/MudBlazor.Markdown.js",
+		 "./Resources/MudBlazor.Markdown.css"
+		],
 	output: {
 		path: path.resolve(__dirname, "wwwroot"),
-		filename: "MudBlazor.Markdown.min.js"
-	}
+		filename: "MudBlazor.Markdown.min.js",
+		publicPath: "/"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					"css-loader"
+				]
+			},
+		]
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: "MudBlazor.Markdown.min.css"
+		})
+	],
 };
