4ab35cf22bce89a818a669f033becee7844f204d
mihails.kuzmins <mihails.kuzmins@daytongroup.lv>
2021-09-12 21:50:31 +0300

    first as webpack

---
+++ src/MudBlazor.Markdown/Components/MudCodeHighlight.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components.Rendering;
+
+// ReSharper disable once CheckNamespace
+namespace MudBlazor
+{
+	internal sealed class MudCodeHighlight : MudComponentBase
+	{
+		protected override void BuildRenderTree(RenderTreeBuilder builder)
+		{
+
+		}
+	}
+}
