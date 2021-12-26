cd260fcfd0f1cca0b316a1e319ae78bc6480e84e
ミシャ <47413092+MihailsKuzmins@users.noreply.github.com>
2021-12-26 21:45:31 +0200

    copy code-style

--- src/MudBlazor.Markdown/gulpfile.js
+++ src/MudBlazor.Markdown/gulpfile.js
@@ -1,20 +1,31 @@
 const { src, dest, series } = require("gulp");
 const webpack = require("webpack-stream");
 const rename = require("gulp-rename");
-const minifyCss = require("gulp-minify-css");
+const minifyCss = require("gulp-clean-css");
+const changeCase = require("change-case");
 
-function css() {
-    return src("Resources/*.css")
-        .pipe(minifyCss())
-        .pipe(rename({ extname: ".min.css" }))
-        .pipe(dest("wwwroot"));
+function cssMain() {
+	return src("Resources/*.css")
+		.pipe(minifyCss())
+		.pipe(rename({ extname: ".min.css" }))
+		.pipe(dest("wwwroot"));
+}
+
+function cssCodeStyles() {
+	return src("Resources/CodeStyles/src/**/*.css")
+		.pipe(minifyCss({ level: { 1: { specialComments: "0" } } }))
+		.pipe(rename(function (path) {
+			path.dirname = changeCase.camelCase(path.dirname);
+			path.extname = ".min.css";
+		}))
+		.pipe(dest("wwwroot/code-styles"));
 }
 
 function js() {
-    return src("Resources/MudBlazor.Markdown.js")
-        .pipe(webpack())
-        .pipe(rename({ basename: "MudBlazor.Markdown", extname: ".min.js" }))
-        .pipe(dest("wwwroot"));
+	return src("Resources/MudBlazor.Markdown.js")
+		.pipe(webpack())
+		.pipe(rename({ basename: "MudBlazor.Markdown", extname: ".min.js" }))
+		.pipe(dest("wwwroot"));
 }
 
-exports.default = series(css, js);
+exports.default = series(cssMain, cssCodeStyles, js);
