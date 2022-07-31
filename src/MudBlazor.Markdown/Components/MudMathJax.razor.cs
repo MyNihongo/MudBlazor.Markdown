03553613348e4eab345df55fef20399a9093778d
ミシャ <47413092+MihailsKuzmins@users.noreply.github.com>
2022-07-31 11:06:41 +0900

    add fonts

--- src/MudBlazor.Markdown/Components/MudMathJax.razor.cs
+++ src/MudBlazor.Markdown/Components/MudMathJax.razor.cs
@@ -1,7 +1,10 @@
-﻿namespace MudBlazor;
+﻿using System.Runtime.CompilerServices;
+
+namespace MudBlazor;
 
 internal sealed class MudMathJax : ComponentBase
 {
+	private const string SpacingClass = "pl-2";
 	private int _elementIndex;
 
 	[Parameter]
@@ -14,11 +17,55 @@ internal sealed class MudMathJax : ComponentBase
 		builder.OpenElement(_elementIndex++, "mjx-container");
 		builder.AddAttribute(_elementIndex++, "tabindex", "0");
 		builder.AddAttribute(_elementIndex++, "class", "mud-markdown-mjx-container");
-
-		builder.AddMarkupContent(_elementIndex++, "<mi>x</mi><mo>=</mo><mi>y</mi>");
-
+		BuildMarkupContent(builder, Value.AsSpan());
 		builder.CloseComponent();
 	}
 
+	private void BuildMarkupContent(RenderTreeBuilder builder, ReadOnlySpan<char> value)
+	{
+		for (var i = 0; i < value.Length; i++)
+		{
+			if (i + 1 < value.Length && value[i + 1] == '^')
+			{
+				i++;
+			}
+			else if (value[i].IsAlpha())
+			{
+				RenderAlpha(builder, value[i]);
+			}
+			else if (value[i].IsDigit())
+			{
+				RenderDigit(builder, value[i]);
+			}
+			else if (value[i].IsMathOperation())
+			{
+				RenderMathOperation(builder, value[i]);
+			}
+		}
+	}
 
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	private void RenderAlpha(in RenderTreeBuilder builder, in char value)
+	{
+		builder.OpenElement(_elementIndex++, "mi");
+		builder.AddContent(_elementIndex++, value);
+		builder.CloseComponent();
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	private void RenderDigit(in RenderTreeBuilder builder, in char value)
+	{
+		builder.OpenElement(_elementIndex++, "mn");
+		builder.AddContent(_elementIndex++, value);
+		builder.CloseComponent();
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	private void RenderMathOperation(in RenderTreeBuilder builder, in char value)
+	{
+		builder.OpenElement(_elementIndex++, "mo");
+		builder.AddAttribute(_elementIndex++, "class", $"mud-markdown-mjx-expression {SpacingClass}");
+		builder.AddContent(_elementIndex++, value);
+		builder.CloseComponent();
+	}
 }
---
+++ src/MudBlazor.Markdown/Extensions/CharEx.cs
@@ -0,0 +1,7 @@
+﻿namespace MudBlazor;
+
+internal static class CharEx
+{
+	public static bool IsMathOperation(this char @this) =>
+		@this is '=' or '>' or '<' or '-' or '+';
+}
