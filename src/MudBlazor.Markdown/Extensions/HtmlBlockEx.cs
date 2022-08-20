72e655188c9ebc498c4fb8866561847cf053074c
ミシャ <47413092+MihailsKuzmins@users.noreply.github.com>
2022-08-20 13:14:07 +0900

    check when inside details

--- src/MudBlazor.Markdown/Extensions/HtmlBlockEx.cs
+++ src/MudBlazor.Markdown/Extensions/HtmlBlockEx.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics;
-using System.Text;
+﻿using System.Text;
 using Markdig.Syntax;
 
 namespace MudBlazor;
@@ -8,18 +7,23 @@ internal static class HtmlBlockEx
 {
 	public static bool TryGetDetails(this HtmlBlock @this, out HtmlDetailsData htmlDetailsData)
 	{
+		htmlDetailsData = new HtmlDetailsData();
+
+		if (!@this.Lines.StartsAndEndsWith("<details>", "</details>"))
+			return false;
+
 		StringBuilder summaryBuilder = new(), detailsBuilder = new();
 		bool isSummary = false, isDetail = false;
 
-		for (var i = 0; i < @this.Lines.Lines.Length; i++)
-		{
-			@this.Lines.Lines[i].Slice
-		}
+		//for (var i = 0; i < @this.Lines.Lines.Length; i++)
+		//{
+		//	@this.Lines.Lines[i].Slice
+		//}
 
-		foreach (var stringLine in @this.Lines.Lines)
-		{
-			Debug.WriteLine(stringLine);
-		}
+		//foreach (var stringLine in @this.Lines.Lines)
+		//{
+		//	Debug.WriteLine(stringLine);
+		//}
 
 		htmlDetailsData = new HtmlDetailsData();
 		return false;
---
+++ src/MudBlazor.Markdown/Extensions/StringLineGroupEx.cs
@@ -0,0 +1,29 @@
+﻿namespace MudBlazor;
+
+internal static class StringLineGroupEx
+{
+	public static bool StartsAndEndsWith(this StringLineGroup @this, in string startsWith, in string endsWith)
+	{
+		if (@this.Count == 0)
+			return false;
+
+		// Starts with
+		if (@this.Lines[0].Slice.Length < startsWith.Length)
+			return false;
+
+		for (var i = 0; i < startsWith.Length; i++)
+			if (@this.Lines[0].Slice[i] != startsWith[i])
+				return false;
+
+		// Ends with
+		var lastLine = @this.Lines[@this.Count - 1];
+		if (lastLine.Slice.Length < endsWith.Length)
+			return false;
+
+		for (int i = endsWith.Length - 1, j = lastLine.Slice.Length - 1; i >= 0; i--, j--)
+			if (endsWith[i] != lastLine.Slice[lastLine.Position + j])
+				return false;
+
+		return true;
+	}
+}
