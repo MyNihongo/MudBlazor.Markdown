9b65961a8a168130d0c26360e7db2de5aa24a820
ミシャ <47413092+MihailsKuzmins@users.noreply.github.com>
2022-08-28 19:00:50 +0900

    remove font imports

--- src/MudBlazor.Markdown/Extensions/MathInlineEx.cs
+++ src/MudBlazor.Markdown/Extensions/MathInlineEx.cs
@@ -4,12 +4,10 @@ namespace MudBlazor;
 
 internal static class MathInlineEx
 {
-	public static string GetDelimiter(this MathInline @this)
-	{
-		return string.Create(@this.DelimiterCount, @this.Delimiter, static (span, c) =>
+	public static string GetDelimiter(this MathInline @this) =>
+		string.Create(@this.DelimiterCount, @this.Delimiter, static (span, c) =>
 		{
 			for (var i = 0; i < span.Length; i++)
 				span[i] = c;
 		});
-	}
 }
