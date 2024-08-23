7c439ef3ca9aae0a34c7fc2d28cb87684bfdc050
kuzmins-85 <kuzmins-85@soft-gear.co.jp>
2024-08-23 11:56:04 +0900

    fix some tests

--- tests/MudBlazor.Markdown.Tests/MarkdownComponentTests/MarkdownComponentDetailsShould.cs
+++ tests/MudBlazor.Markdown.Tests/MarkdownComponentTests/MarkdownComponentDetailsShould.cs
@@ -19,12 +19,13 @@ public sealed class MarkdownComponentDetailsShould : MarkdownComponentTestsBase
 			<div class='mud-expand-panel-text'>
 				<p class='mud-typography mud-typography-body1'>Header</p>
 			</div>
-			<svg class='mud-icon-root mud-svg-icon mud-icon-size-medium mud-expand-panel-icon' focusable='false' viewBox='0 0 24 24' aria-hidden='true'>
-				<path d='M0 0h24v24H0z' fill='none'/><path d='M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z'/>
+			<svg class='mud-icon-root mud-svg-icon mud-icon-size-medium mud-expand-panel-icon' focusable='false' viewBox='0 0 24 24' aria-hidden='true' role='img'>
+				<path d='M0 0h24v24H0z' fill='none'/>
+				<path d='M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z'/>
 			</svg>
 		</div>
-		<div class='mud-collapse-container' style='' blazor:elementReference='608e6c71-e73b-49e8-bbf5-30bbb613ad11'>
-			<div class='mud-collapse-wrapper' blazor:elementReference='e1b1f0e7-04f7-4696-8279-3918626ffde7'>
+		<div aria-expanded='false' blazor:onanimationend='2' class='mud-collapse-container' style=''>
+			<div class='mud-collapse-wrapper' blazor:elementReference='f0339543-23a6-43d6-92b9-b8b55ec88622'>
 				<div class='mud-collapse-wrapper-inner'>
 					<div class='mud-expand-panel-content'>
 						<p class='mud-typography mud-typography-body1'>Some hidden text<br />Another text</p>
@@ -58,11 +59,12 @@ public sealed class MarkdownComponentDetailsShould : MarkdownComponentTestsBase
 			<div class='mud-expand-panel-text'>
 				<p class='mud-typography mud-typography-body1'>Header</p>
 			</div>
-			<svg class='mud-icon-root mud-svg-icon mud-icon-size-medium mud-expand-panel-icon' focusable='false' viewBox='0 0 24 24' aria-hidden='true'>
-				<path d='M0 0h24v24H0z' fill='none'/><path d='M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z'/>
+			<svg class='mud-icon-root mud-svg-icon mud-icon-size-medium mud-expand-panel-icon' focusable='false' viewBox='0 0 24 24' aria-hidden='true' role='img'>
+				<path d='M0 0h24v24H0z' fill='none'/>
+				<path d='M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z'/>
 			</svg>
 		</div>
-		<div class='mud-collapse-container' style='' blazor:elementReference='d178b8fb-4478-4ada-9d52-25cccd6e50b8'>
+		<div aria-expanded='false' blazor:onanimationend='2' class='mud-collapse-container' style=''>
 			<div class='mud-collapse-wrapper' blazor:elementReference='99a17eaa-605d-411d-968b-457a412ddd48'>
 				<div class='mud-collapse-wrapper-inner'>
 					<div class='mud-expand-panel-content'>
@@ -82,7 +84,7 @@ public sealed class MarkdownComponentDetailsShould : MarkdownComponentTestsBase
 	public void RenderDetailsInline()
 	{
 		const string value =
-@"<details><summary>Header</summary>Some hidden text</details>";
+"<details><summary>Header</summary>Some hidden text</details>";
 
 		const string expected =
 @"<article class='mud-markdown-body'>
@@ -91,12 +93,13 @@ public sealed class MarkdownComponentDetailsShould : MarkdownComponentTestsBase
 			<div class='mud-expand-panel-text'>
 				<p class='mud-typography mud-typography-body1'>Header</p>
 			</div>
-			<svg class='mud-icon-root mud-svg-icon mud-icon-size-medium mud-expand-panel-icon' focusable='false' viewBox='0 0 24 24' aria-hidden='true'>
-				<path d='M0 0h24v24H0z' fill='none'/><path d='M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z'/>
+			<svg class='mud-icon-root mud-svg-icon mud-icon-size-medium mud-expand-panel-icon' focusable='false' viewBox='0 0 24 24' aria-hidden='true' role='img'>
+				<path d='M0 0h24v24H0z' fill='none'/>
+				<path d='M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z'/>
 			</svg>
 		</div>
-		<div class='mud-collapse-container' style='' blazor:elementReference='8c13559f-c8c5-4ee3-bf41-5693b88ea39a'>
-			<div class='mud-collapse-wrapper' blazor:elementReference='5939b12a-7237-44d9-92a5-bbc235724ee5'>
+		<div aria-expanded='false' blazor:onanimationend='2' class='mud-collapse-container' style=''>
+			<div class='mud-collapse-wrapper' blazor:elementReference='68fe8a2c-e680-4a80-8e06-e62217d04ef8'>
 				<div class='mud-collapse-wrapper-inner'>
 					<div class='mud-expand-panel-content'>
 						<p class='mud-typography mud-typography-body1'>Some hidden tex</p>
@@ -155,11 +158,12 @@ public sealed class MarkdownComponentDetailsShould : MarkdownComponentTestsBase
 			<div class='mud-expand-panel-text'>
 				<p class='mud-typography mud-typography-body1'>Header</p>
 			</div>
-			<svg class='mud-icon-root mud-svg-icon mud-icon-size-medium mud-expand-panel-icon mud-transform' focusable='false' viewBox='0 0 24 24' aria-hidden='true'>
-				<path d='M0 0h24v24H0z' fill='none'/><path d='M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z'/>
+			<svg class='mud-icon-root mud-svg-icon mud-icon-size-medium mud-expand-panel-icon mud-transform' focusable='false' viewBox='0 0 24 24' aria-hidden='true' role='img'>
+				<path d='M0 0h24v24H0z' fill='none'/>
+				<path d='M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z'/>
 			</svg>
 		</div>
-		<div class='mud-collapse-container mud-collapse-entering' style='height:0px;animation-duration:s;' blazor:elementReference=''>
+		<div aria-expanded='true' blazor:onanimationend='4' class='mud-collapse-container mud-collapse-entering' style='height:0px;animation-duration:s;'>
 			<div class='mud-collapse-wrapper' blazor:elementReference=''>
 				<div class='mud-collapse-wrapper-inner'>
 					<div class='mud-expand-panel-content'>
@@ -200,11 +204,12 @@ public sealed class MarkdownComponentDetailsShould : MarkdownComponentTestsBase
 			<div class='mud-expand-panel-text'>
 				<p class='mud-typography mud-typography-body1'>First</p>
 			</div>
-			<svg class='mud-icon-root mud-svg-icon mud-icon-size-medium mud-expand-panel-icon' focusable='false' viewBox='0 0 24 24' aria-hidden='true'>
-				<path d='M0 0h24v24H0z' fill='none'/><path d='M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z'/>
+			<svg class='mud-icon-root mud-svg-icon mud-icon-size-medium mud-expand-panel-icon' focusable='false' viewBox='0 0 24 24' aria-hidden='true' role='img'>
+				<path d='M0 0h24v24H0z' fill='none'/>
+				<path d='M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z'/>
 			</svg>
 		</div>
-		<div class='mud-collapse-container' style='' blazor:elementReference='6f16798d-aede-4e29-bd50-af3971a209bd'>
+		<div aria-expanded='false' blazor:onanimationend='2' class='mud-collapse-container' style=''>
 			<div class='mud-collapse-wrapper' blazor:elementReference='48c895d6-251f-406a-b7f1-62609e7e2256'>
 				<div class='mud-collapse-wrapper-inner'>
 					<div class='mud-expand-panel-content'>
@@ -213,11 +218,11 @@ public sealed class MarkdownComponentDetailsShould : MarkdownComponentTestsBase
 								<div class='mud-expand-panel-text'>
 									<p class='mud-typography mud-typography-body1'>Second</p>
 								</div>
-								<svg class='mud-icon-root mud-svg-icon mud-icon-size-medium mud-expand-panel-icon' focusable='false' viewBox='0 0 24 24' aria-hidden='true'>
+								<svg class='mud-icon-root mud-svg-icon mud-icon-size-medium mud-expand-panel-icon' focusable='false' viewBox='0 0 24 24' aria-hidden='true' role='img'>
 									<path d='M0 0h24v24H0z' fill='none'/><path d='M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z'/>
 								</svg>
 							</div>
-							<div class='mud-collapse-container' style='' blazor:elementReference='ce2aa8a6-a1c8-4c5b-8a61-0146cc10dbe0'>
+							<div aria-expanded='false' blazor:onanimationend='2' class='mud-collapse-container' style=''>
 								<div class='mud-collapse-wrapper' blazor:elementReference='97064b7c-1652-472e-89cf-ebe6cc63dee9'>
 									<div class='mud-collapse-wrapper-inner'>
 										<div class='mud-expand-panel-content'>
@@ -226,11 +231,11 @@ public sealed class MarkdownComponentDetailsShould : MarkdownComponentTestsBase
 													<div class='mud-expand-panel-text'>
 														<p class='mud-typography mud-typography-body1'>Third</p>
 													</div>
-													<svg class='mud-icon-root mud-svg-icon mud-icon-size-medium mud-expand-panel-icon' focusable='false' viewBox='0 0 24 24' aria-hidden='true'>
+													<svg class='mud-icon-root mud-svg-icon mud-icon-size-medium mud-expand-panel-icon' focusable='false' viewBox='0 0 24 24' aria-hidden='true' role='img'>
 														<path d='M0 0h24v24H0z' fill='none'/><path d='M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z'/>
 													</svg>
 												</div>
-												<div class='mud-collapse-container' style='' blazor:elementReference='fa3498ed-173b-4c61-901c-23f434cd585f'>
+												<div aria-expanded='false' blazor:onanimationend='2' class='mud-collapse-container' style=''>
 													<div class='mud-collapse-wrapper' blazor:elementReference='b051f40f-6621-407e-ba8d-c0be30338c31'>
 														<div class='mud-collapse-wrapper-inner'>
 															<div class='mud-expand-panel-content'>
