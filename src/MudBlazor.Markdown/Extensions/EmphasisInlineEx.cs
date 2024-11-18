ebb342925093a98827bfe1997837b87254b0a910
NAL <niko.a.leinonen@gmail.com>
2024-11-18 20:24:20 +0200

    Implement strikethrough

--- src/MudBlazor.Markdown/Extensions/EmphasisInlineEx.cs
+++ src/MudBlazor.Markdown/Extensions/EmphasisInlineEx.cs
@@ -6,7 +6,10 @@ internal static class EmphasisInlineEx
 {
 	public static bool TryGetEmphasisElement(this EmphasisInline emphasis, out string value)
 	{
-		const string italics = "i", bold = "b";
+		const string
+			italics = "i",
+			bold = "b",
+			strike = "del";
 
 		value = emphasis.DelimiterChar switch
 		{
@@ -17,6 +20,11 @@ internal static class EmphasisInlineEx
 				_ => string.Empty
 			},
 			'_' => italics,
+			'~' => emphasis.DelimiterCount switch
+			{
+				2 => strike,
+				_ => string.Empty
+			},
 			_ => string.Empty
 		};
 
