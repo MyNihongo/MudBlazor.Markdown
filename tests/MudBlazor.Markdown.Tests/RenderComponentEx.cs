0baf3cb6b9fa8b007027ac6cd023b0bfff236152
mihails.kuzmins <mihails.kuzmins@daytongroup.lv>
2025-04-05 14:47:17 +0900

    tests for url

--- tests/MudBlazor.Markdown.Tests/RenderComponentEx.cs
+++ tests/MudBlazor.Markdown.Tests/RenderComponentEx.cs
@@ -2,10 +2,10 @@ namespace MudBlazor.Markdown.Tests;
 
 public static class RenderComponentEx
 {
-	public static IRenderedComponent<T> AwaitElement<T>(this IRenderedComponent<T> @this, in string cssSelector)
+	public static IRenderedComponent<T> AwaitElement<T>(this IRenderedComponent<T> @this, in string cssSelector, in TimeSpan? timeout = null)
 		where T : ComponentBase
 	{
-		@this.WaitForElement(cssSelector, TimeSpan.FromSeconds(0.5d));
+		@this.WaitForElement(cssSelector, timeout ?? TimeSpan.FromSeconds(0.5d));
 		return @this;
 	}
 }
