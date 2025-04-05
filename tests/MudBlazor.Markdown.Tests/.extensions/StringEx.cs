29dc95f5896061f7adf9f9d30ca597a322598e53
mihails.kuzmins <mihails.kuzmins@daytongroup.lv>
2025-04-05 16:06:37 +0900

    append home to the path

--- tests/MudBlazor.Markdown.Tests/.extensions/StringEx.cs
+++ tests/MudBlazor.Markdown.Tests/.extensions/StringEx.cs
@@ -7,7 +7,11 @@ public static class StringEx
 	public static string EscapePath(this string @this)
 	{
 		var root = Path.GetPathRoot(Environment.CurrentDirectory) ?? throw new Exception($"Cannot get the root path, string=`{@this}`");
-		
+
+		// On linux the solution is build in "/home"
+		if (root == "/")
+			root += "home";
+
 		const StringComparison stringComparison = StringComparison.OrdinalIgnoreCase;
 		var stringBuilder = new StringBuilder(@this.Length);
 		var span = @this.AsSpan();
@@ -18,15 +22,15 @@ public static class StringEx
 			stringBuilder
 				.Append(span[startIndex..endIndex])
 				.Append("{path}");
-			
+
 			const string extension = ".md";
 			startIndex = @this.IndexOf(extension, endIndex, stringComparison);
 			if (startIndex == -1)
 				throw new Exception($"Cannot find the end of the path, string=`{@this}`, index=`{endIndex}`");
-			
+
 			startIndex += extension.Length;
 		}
-		
+
 		return stringBuilder
 			.Append(span[startIndex..])
 			.ToString();
