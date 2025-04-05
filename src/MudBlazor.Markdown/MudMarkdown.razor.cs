a5f79c9accdbdd4472562978ad8136f5956fe3c0
mihails.kuzmins <mihails.kuzmins@daytongroup.lv>
2025-04-05 14:36:49 +0900

    undo

--- src/MudBlazor.Markdown/MudMarkdown.razor.cs
+++ src/MudBlazor.Markdown/MudMarkdown.razor.cs
@@ -106,9 +106,10 @@ public class MudMarkdown : ComponentBase, IDisposable
 		if (parameters.TryGetValue<string>(nameof(Value), out var value) && !ReferenceEquals(Value, value))
 		{
 			var sourceType = parameters.GetValueOrDefault<MarkdownSourceType>(nameof(SourceType));
+			var dictionary = parameters.ToMutableDictionary(); // must be before the `await` call
+
 			Value = await MudMarkdownValueProvider.GetValueAsync(value, sourceType);
 
-			var dictionary = parameters.ToMutableDictionary();
 			dictionary[nameof(Value)] = Value;
 			parameters = ParameterView.FromDictionary(dictionary);
 		}
