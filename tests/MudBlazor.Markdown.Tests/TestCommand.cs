58d679287f364af04674cb743610cfe48793a807
mihails.kuzmins <mihails.kuzmins@daytongroup.lv>
2025-04-05 16:31:22 +0900

    undo pragma

--- tests/MudBlazor.Markdown.Tests/TestCommand.cs
+++ tests/MudBlazor.Markdown.Tests/TestCommand.cs
@@ -2,7 +2,9 @@
 
 internal sealed class TestCommand : ICommand
 {
+#pragma warning disable 67
 	public event EventHandler? CanExecuteChanged;
+#pragma warning restore 67
 
 	public bool CanExecute(object? parameter) =>
 		true;
