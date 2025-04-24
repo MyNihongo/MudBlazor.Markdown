8374c869a378caf70c61b3c946db31bea5661a17
MihailsKuzmins <47413092+MihailsKuzmins@users.noreply.github.com>
2025-04-24 14:31:20 +0900

    add tests

---
+++ tests/MudBlazor.Markdown.Tests/Extensions/CodeBlockExTests/CreateCodeBlockTextShould.cs
@@ -0,0 +1,43 @@
+﻿namespace MudBlazor.Markdown.Tests.Extensions.CodeBlockExTests;
+
+public sealed class CreateCodeBlockTextShould : CodeBlockExTestsBase
+{
+	[Fact]
+	public void ReturnTextForSingleLine()
+	{
+		const string input = "singleline";
+		
+		var result = CreateFixture(input)
+			.CreateCodeBlockText();
+		
+		result
+			.Should()
+			.Be(input);
+	}
+	
+	[Fact]
+	public void ReturnTextForMultiLines()
+	{
+		const string input = "first line\r\nnext line";
+		
+		var result = CreateFixture(input)
+			.CreateCodeBlockText();
+		
+		result
+			.Should()
+			.Be(input);
+	}
+	
+	[Fact]
+	public void ReturnTextWithEmptyLine()
+	{
+		const string input = "first line\r\n\r\nnext line";
+		
+		var result = CreateFixture(input)
+			.CreateCodeBlockText();
+		
+		result
+			.Should()
+			.Be(input);
+	}
+}
