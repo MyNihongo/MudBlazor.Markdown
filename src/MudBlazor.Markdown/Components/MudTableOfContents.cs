a69a19b6be256f78e2f1cb64a22b72b4f35c8898
kuzmins <kuzmins@vesca.co.jp>
2025-05-15 11:41:47 +0900

    create a sample table of contents

---
+++ src/MudBlazor.Markdown/Components/MudTableOfContents.cs
@@ -0,0 +1,71 @@
+﻿namespace MudBlazor;
+
+internal sealed class MudTableOfContents : ComponentBase
+{
+	private bool _openRight = true;
+	
+	[Parameter]
+	public RenderFragment? ChildContent { get; set; }
+
+	protected override void BuildRenderTree(RenderTreeBuilder builder1)
+	{
+		var elementIndex1 = 0;
+
+		builder1.OpenElement(elementIndex1++, ElementNames.Div);
+		// TODO: style as class
+		builder1.AddAttribute(elementIndex1++, AttributeNames.Style, "overflow:hidden; position:relative;");
+		builder1.OpenComponent<MudDrawerContainer>(elementIndex1++);
+		builder1.AddComponentParameter(elementIndex1++, nameof(MudDrawerContainer.Class), "mud-height-full");
+		builder1.AddAttribute(elementIndex1, nameof(MudDrawerContainer.ChildContent), (RenderFragment)(builder2 =>
+				{
+			var elementIndex2 = 0;
+			builder2.OpenComponent<MudDrawer>(elementIndex2++);
+			builder2.AddComponentParameter(elementIndex2++, nameof(MudDrawer.Fixed), false);
+			builder2.AddComponentParameter(elementIndex2++, nameof(MudDrawer.Anchor), Anchor.Right);
+			builder2.AddComponentParameter(elementIndex2++, nameof(MudDrawer.Elevation), 0);
+			builder2.AddComponentParameter(elementIndex2++, nameof(MudDrawer.Variant), DrawerVariant.Persistent);
+			builder2.AddComponentParameter(elementIndex2++, nameof(MudDrawer.Open), _openRight);
+			builder2.AddComponentParameter(elementIndex2++, nameof(MudDrawer.OpenChanged), EventCallback.Factory.Create(this, RuntimeHelpers.CreateInferredEventCallback(this, isOpen =>
+			{
+				_openRight = isOpen;
+			}, _openRight)));
+			builder2.AddAttribute(elementIndex2++, nameof(MudDrawer.ChildContent), (RenderFragment)(builder3 =>
+			{
+				var elementIndex3 = 0;
+				builder3.OpenComponent<MudDrawerHeader>(elementIndex3++);
+				builder3.AddAttribute(elementIndex3++, nameof(MudDrawerHeader.ChildContent), (RenderFragment)(builder4 =>
+				{
+					var elementIndex4 = 0;
+					builder4.OpenComponent<MudText>(elementIndex4++);
+					builder4.AddComponentParameter(elementIndex4++, nameof(MudText.Typo), Typo.h6);
+					builder4.AddAttribute(elementIndex4, nameof(MudText.ChildContent), (RenderFragment)(builder5 =>
+					{
+						builder5.AddContent(0, "Table of contents");
+					}));
+					builder4.CloseComponent();
+				}));
+				builder3.CloseComponent();
+				builder3.OpenComponent<MudNavMenu>(elementIndex3++);
+				builder3.AddAttribute(elementIndex3, nameof(MudNavMenu.ChildContent), (RenderFragment)(builder4 =>
+				{
+					var elementIndex4 = 0;
+					builder4.OpenComponent<MudText>(elementIndex4++);
+					builder4.AddAttribute(elementIndex4, nameof(MudText.ChildContent), (RenderFragment)delegate(RenderTreeBuilder builder5)
+					{
+						builder5.AddContent(0, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ullamcorper orci at mi pellentesque scelerisque. Nullam pulvinar felis elit, varius ultrices urna convallis aliquet. Aliquam magna nibh, blandit a sapien non, pretium rutrum turpis. Vestibulum rhoncus nibh et consequat hendrerit. Duis pretium nulla libero, euismod venenatis nunc mattis ac. Ut luctus sapien quis consequat facilisis. Aliquam at nibh elit. Nunc et cursus arcu.");
+					});
+					builder4.CloseComponent();
+				}));
+				builder3.CloseComponent();
+			}));
+			builder2.CloseComponent();
+			
+			builder2.OpenElement(elementIndex2++, ElementNames.Div);
+			builder2.AddAttribute(elementIndex2++, AttributeNames.Class, "d-flex mud-height-full");
+			builder2.AddContent(elementIndex2, ChildContent);
+			builder2.CloseElement();
+		}));
+		builder1.CloseComponent();
+		builder1.CloseElement();
+	}
+}
