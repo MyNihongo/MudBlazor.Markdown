364414589465931804bc1ccc015886a5255ed37a
kuzmins <kuzmins@vesca.co.jp>
2025-06-03 15:57:18 +0900

    render headings

--- src/MudBlazor.Markdown/Components/MudTableOfContents.cs
+++ src/MudBlazor.Markdown/Components/MudTableOfContents.cs
@@ -15,6 +15,14 @@ internal sealed class MudTableOfContents : ComponentBase
 	[Parameter]
 	public RenderFragment<MudMarkdownHeadingTree>? ChildContent { get; set; }
 
+	private string DrawerClass => new CssBuilder("mud-markdown-toc-drawer-content")
+		.AddClass("open", _isOpen)
+		.Build();
+
+	private string ContentClass => new CssBuilder("mud-markdown-toc-content")
+		.AddClass("open", _isOpen)
+		.Build();
+
 	protected override void BuildRenderTree(RenderTreeBuilder builder1)
 	{
 		var markdownHeadingTree = new MudMarkdownHeadingTree();
@@ -41,19 +49,39 @@ internal sealed class MudTableOfContents : ComponentBase
 			builder2.CloseComponent();
 
 			// Drawer
-			var @class = new CssBuilder("mud-markdown-toc-drawer-content").AddClass("open", _isOpen).Build();
 			builder2.OpenElement(elementIndex2++, ElementNames.Div);
-			builder2.AddAttribute(elementIndex2++, AttributeNames.Class, @class);
-			builder2.AddContent(elementIndex2++, "aaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaa aaaaaaaaaaaa");
+			builder2.AddAttribute(elementIndex2++, AttributeNames.Class, DrawerClass);
+			builder2.AddContent(elementIndex2, (RenderFragment)(builder3 =>
+			{
+				var elementIndex3 = 0;
+
+				if (!string.IsNullOrEmpty(Header))
+				{
+					builder3.OpenComponent<MudDrawerHeader>(elementIndex3++);
+					builder3.AddAttribute(elementIndex3++, nameof(MudDrawerHeader.ChildContent), (RenderFragment)(builder4 =>
+					{
+						var elementIndex4 = 0;
+						builder4.OpenComponent<MudText>(elementIndex4++);
+						builder4.AddComponentParameter(elementIndex4++, nameof(MudText.Typo), Typo.h6);
+						builder4.AddAttribute(elementIndex4, nameof(MudText.ChildContent), (RenderFragment)(builder5 => builder5.AddContent(0, Header)));
+						builder4.CloseComponent();
+					}));
+					builder3.CloseComponent();
+				}
+
+				builder3.OpenComponent<MudTableOfContentsNavMenu>(elementIndex3++);
+				builder3.AddComponentParameter(elementIndex3++, nameof(MudTableOfContentsNavMenu.MarkdownHeadingTree), markdownHeadingTree);
+				builder3.AddComponentParameter(elementIndex3, nameof(MudTableOfContentsNavMenu.MarkdownComponentId), MarkdownComponentId);
+				builder3.CloseComponent();
+			}));
 			builder2.CloseElement();
 		}));
 		builder1.CloseElement();
 
 		// Content
-		var @class = new CssBuilder("mud-markdown-toc-content").AddClass("open", _isOpen).Build();
 		builder1.OpenElement(elementIndex1++, ElementNames.Div);
-		builder1.AddAttribute(elementIndex1++, AttributeNames.Class, @class);
-		builder1.AddContent(elementIndex1++, ChildContent?.Invoke(markdownHeadingTree));
+		builder1.AddAttribute(elementIndex1++, AttributeNames.Class, ContentClass);
+		builder1.AddContent(elementIndex1, ChildContent?.Invoke(markdownHeadingTree));
 		builder1.CloseElement();
 
 		builder1.CloseElement();
