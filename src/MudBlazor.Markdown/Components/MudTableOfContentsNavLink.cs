0fbee9c93536f72166594ce845426f8ef5398e34
mihails.kuzmins <mihails.kuzmins@daytongroup.lv>
2025-06-06 20:05:01 +0900

    inherit just from ComponentBase

--- src/MudBlazor.Markdown/Components/MudTableOfContentsNavLink.cs
+++ src/MudBlazor.Markdown/Components/MudTableOfContentsNavLink.cs
@@ -2,7 +2,7 @@
 
 namespace MudBlazor;
 
-internal sealed class MudTableOfContentsNavLink : MudComponentBase
+internal sealed class MudTableOfContentsNavLink : ComponentBase
 {
 	[Parameter]
 	public string? Id { get; set; }
@@ -19,18 +19,18 @@ internal sealed class MudTableOfContentsNavLink : MudComponentBase
 	[Parameter]
 	public Func<string, Task>? OnClick { get; set; }
 
+	private string Class => new CssBuilder($"mud-nav-link-{Typo}")
+		.AddClass("active", IsActive)
+		.Build();
+
 	protected override void BuildRenderTree(RenderTreeBuilder builder1)
 	{
 		var elementIndex1 = 0;
 		builder1.OpenElement(elementIndex1++, ElementNames.Nav);
 		builder1.AddAttribute(elementIndex1++, AttributeNames.Class, "mud-nav-group");
 
-		var @class = new CssBuilder($"mud-nav-link-{Typo}")
-			.AddClass("active", IsActive)
-			.Build();
-
 		builder1.OpenComponent<MudNavLink>(elementIndex1++);
-		builder1.AddComponentParameter(elementIndex1++, nameof(MudNavLink.Class), @class);
+		builder1.AddComponentParameter(elementIndex1++, nameof(MudNavLink.Class), Class);
 		builder1.AddComponentParameter(elementIndex1++, nameof(MudNavLink.OnClick), EventCallback.Factory.Create<MouseEventArgs>(this, OnClickAsync));
 		builder1.AddComponentParameter(elementIndex1, nameof(MudNavLink.ChildContent), (RenderFragment)(builder2 => builder2.AddContent(0, Title)));
 		builder1.CloseComponent();
