c2295f05b79b6ced73efccd688fbdc881c9e7421
MihailsKuzmins <47413092+MihailsKuzmins@users.noreply.github.com>
2025-07-05 17:37:53 +0900

    add Dense styling for the table (#326)

--- src/MudBlazor.Markdown/Helpers/MudMarkdownStyling.cs
+++ src/MudBlazor.Markdown/Helpers/MudMarkdownStyling.cs
@@ -25,23 +25,43 @@ public sealed class MudMarkdownStyling
 		/// <summary>
 		/// If true, striped table rows will be used.
 		/// </summary>
+		/// <remarks>
+		/// Defaults to <c>true</c>.
+		/// </remarks>
 		public bool IsStriped { get; set; } = true;
 
 		/// <summary>
 		/// If true, table's cells will have left/right borders.
 		/// </summary>
+		/// <remarks>
+		/// Defaults to <c>true</c>.
+		/// </remarks>
 		public bool IsBordered { get; set; } = true;
 
 		/// <summary>
 		/// Child content of component.
 		/// </summary>
+		/// <remarks>
+		/// Defaults to <c>1</c>.
+		/// </remarks>
 		public int Elevation { set; get; } = 1;
 
 		/// <summary>
 		/// Minimum width (in pixels) for a table cell.<br/>
 		/// If <see langword="null" /> or negative the minimum width is not applied.
 		/// </summary>
+		/// <remarks>
+		/// Defaults to <c>null</c>.
+		/// </remarks>
 		public int? CellMinWidth { get; set; }
+
+		/// <summary>
+		/// Uses compact padding for all rows.
+		/// </summary>
+		/// <remarks>
+		/// Defaults to <c>false</c>.
+		/// </remarks>
+		public bool Dense { get; set; }
 	}
 
 	/// <summary>
@@ -52,6 +72,9 @@ public sealed class MudMarkdownStyling
 		/// <summary>
 		/// Underline style.
 		/// </summary>
+		/// <remarks>
+		/// Defaults to <c>Hover</c>.
+		/// </remarks>
 		public Underline Underline { get; set; } = Underline.Hover;
 	}
 
