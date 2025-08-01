cb65480943de3304beb31586638a5c104db4ab57
kuzmins <kuzmins@vesca.co.jp>
2025-08-01 15:51:00 +0900

    display copied

--- src/MudBlazor.Markdown/Components/MudCodeHighlightCopyButton.razor.cs
+++ src/MudBlazor.Markdown/Components/MudCodeHighlightCopyButton.razor.cs
@@ -4,6 +4,9 @@ namespace MudBlazor;
 
 internal sealed class MudCodeHighlightCopyButton : ComponentBase
 {
+	private static readonly TimeSpan IsCopiedResetTimeSpan = TimeSpan.FromSeconds(1.25d);
+	private bool _isCopied;
+
 	[Parameter]
 	public string? Class { get; init; }
 
@@ -21,32 +24,48 @@ internal sealed class MudCodeHighlightCopyButton : ComponentBase
 		var elementIndex = 0;
 
 		builder.OpenComponent<MudIconButton>(elementIndex++);
-		builder.AddComponentParameter(elementIndex++, nameof(MudIconButton.Icon), Icons.Material.Rounded.ContentCopy);
+		if (_isCopied)
+		{
+            builder.AddComponentParameter(elementIndex++, nameof(MudIconButton.Icon), Icons.Material.Rounded.DoneAll);
+            builder.AddComponentParameter(elementIndex++, nameof(MudIconButton.Color), Color.Success);
+		}
+		else
+		{
+            builder.AddComponentParameter(elementIndex++, nameof(MudIconButton.Icon), Icons.Material.Rounded.ContentCopy);
+            builder.AddComponentParameter(elementIndex++, nameof(MudIconButton.Color), Color.Primary);
+            builder.AddComponentParameter(elementIndex++, nameof(MudIconButton.OnClick), EventCallback.Factory.Create<MouseEventArgs>(this, CopyTextToClipboardAsync));
+        }
+
 		builder.AddComponentParameter(elementIndex++, nameof(MudIconButton.Variant), Variant.Filled);
-		builder.AddComponentParameter(elementIndex++, nameof(MudIconButton.Color), Color.Primary);
 		builder.AddComponentParameter(elementIndex++, nameof(MudIconButton.Size), Size.Medium);
 		builder.AddComponentParameter(elementIndex++, nameof(MudIconButton.Class), Class);
-		builder.AddComponentParameter(elementIndex, nameof(MudIconButton.OnClick), EventCallback.Factory.Create<MouseEventArgs>(this, CopyTextToClipboardAsync));
 		builder.CloseComponent();
 	}
 
 	private async Task CopyTextToClipboardAsync(MouseEventArgs args)
 	{
-		if (string.IsNullOrEmpty(Text))
+		if (string.IsNullOrEmpty(Text) || _isCopied)
 			return;
 
 		var ok = await Js.CopyTextToClipboardAsync(Text)
 			.ConfigureAwait(false);
 
-		if (ok)
-			return;
-
-		var clipboardService = ServiceProvider?.GetService<IMudMarkdownClipboardService>();
-
-		if (clipboardService != null)
+		if (!ok)
 		{
-			await clipboardService.CopyToClipboardAsync(Text)
-				.ConfigureAwait(false);
+			var clipboardService = ServiceProvider?.GetService<IMudMarkdownClipboardService>();
+
+			if (clipboardService != null)
+			{
+				await clipboardService.CopyToClipboardAsync(Text)
+					.ConfigureAwait(false);
+			}
 		}
+
+		_isCopied = true;
+		await InvokeAsync(StateHasChanged);
+		await Task.Delay(IsCopiedResetTimeSpan);
+
+		_isCopied = false;
+		await InvokeAsync(StateHasChanged);
 	}
 }
