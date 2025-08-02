c8fce7cb1129d7dce7ab91f95f1e2f4e900ff0cc
mihails.kuzmins <mihails.kuzmins@daytongroup.lv>
2025-08-02 14:31:24 +0900

    tests for copy button

--- tests/MudBlazor.Markdown.Tests/ComponentTestsBase.cs
+++ tests/MudBlazor.Markdown.Tests/ComponentTestsBase.cs
@@ -4,19 +4,23 @@ namespace MudBlazor.Markdown.Tests;
 
 public abstract class ComponentTestsBase : IDisposable
 {
+	private readonly Mock<IPopoverService> _popoverServiceMock = new();
+	protected readonly Mock<IJSRuntime> MockJsRuntime = new();
+	protected readonly TestNavigationManager MockNavigationManager = new();
 	protected readonly TestContext Ctx = new();
 
 	protected ComponentTestsBase()
 	{
+		_popoverServiceMock
+			.SetupGet(x => x.PopoverOptions)
+			.Returns(new PopoverOptions());
+
 		Ctx.Services
 			.AddMudMarkdownServices()
 			.AddSingleton(MockJsRuntime.Object)
+			.AddSingleton(_popoverServiceMock.Object)
 			.AddSingleton<NavigationManager>(MockNavigationManager);
 	}
-
-	protected Mock<IJSRuntime> MockJsRuntime { get; } = new();
-
-	protected TestNavigationManager MockNavigationManager { get; } = new();
 
 	public void Dispose()
 	{
