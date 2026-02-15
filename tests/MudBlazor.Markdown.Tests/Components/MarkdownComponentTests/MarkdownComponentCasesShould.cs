831b0b33f00f1910d602a158624958c5ca4fb2f1
mihails.kuzmins <mihails.kuzmins@daytongroup.lv>
2026-02-15 12:48:47 +0900

    adjust a test

--- tests/MudBlazor.Markdown.Tests/Components/MarkdownComponentTests/MarkdownComponentCasesShould.cs
+++ tests/MudBlazor.Markdown.Tests/Components/MarkdownComponentTests/MarkdownComponentCasesShould.cs
@@ -550,7 +550,7 @@ public sealed class MarkdownComponentCasesShould : MarkdownComponentTestsBase
 
 	#endregion
 
-	#region https: //github.com/MyNihongo/MudBlazor.Markdown/issues/274
+	#region https://github.com/MyNihongo/MudBlazor.Markdown/issues/274
 
 	[Fact]
 	public void RenderTableWithWeirdFormat()
@@ -568,20 +568,14 @@ public sealed class MarkdownComponentCasesShould : MarkdownComponentTestsBase
 
 		const string expected =
 			"""
-			<article id:ignore class='mud-markdown-body'>
-				<h3 id='my-table%3A' class='mud-typography mud-typography-h3'>My Table:</h3>
-				<p class='mud-typography mud-typography-body1'>
-					<div class='mud-markdown-error'>
-						| **Column 1**  **Column 2**   | **Column 3**                |
-						|--------------|----------------]-----------------------------|
-						| Row 1, Col 1 | Row 1, Col 2   |                             |
-						|              | Row 2, Col 2   | Row 2, Col 3                |
-						| Row 3, Col 1 |                | ```python    |
-						|              | Row 4, Col 2   | def greet(name):            |
-						| Row 5, Col 1 l                |     return name             |
-						|              |                | ```          |
-					</div>
-				</p>
+			<article id:ignore class="mud-markdown-body">
+			  <h3 id="my-table%3A" class="mud-typography mud-typography-h3">My Table:</h3>
+			  <p class="mud-typography mud-typography-body1">|
+			    <b>Column 1</b>
+			    <b>Column 2</b>
+			    |
+			    <b>Column 3</b>
+			    | |--------------|----------------]-----------------------------| | Row 1, Col 1 | Row 1, Col 2   |                             | |              | Row 2, Col 2   | Row 2, Col 3                | | Row 3, Col 1 |                | ```python    | |              | Row 4, Col 2   | def greet(name):            | | Row 5, Col 1 l                |     return name             | |              |                | ```          |</p>
 			</article>
 			""";
 
