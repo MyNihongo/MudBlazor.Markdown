804cec16e3886ba5fd691eb444de49638a0ec491
kuzmins <kuzmins@vesca.co.jp>
2026-08-17 12:32:19 +0900

    clean js files

--- src/MudBlazor.Markdown/gulpfile.js
+++ src/MudBlazor.Markdown/gulpfile.js
@@ -2,7 +2,6 @@ const { src, dest, series } = require("gulp");
 const webpack = require("webpack-stream");
 const rename = require("gulp-rename");
 const minifyCss = require("gulp-clean-css");
-const changeCase = require("change-case");
 const all = require("gulp-all");
 
 function fonts() {
@@ -17,21 +16,6 @@ function cssMain() {
 		.pipe(dest("wwwroot"));
 }
 
-function cssCodeStyles() {
-	return src("Resources/CodeStyles/src/**/*.css")
-		.pipe(minifyCss({ level: { 1: { specialComments: "0" } } }))
-		.pipe(rename(function (path) {
-			path.dirname = changeCase.camelCase(path.dirname);
-			path.extname = ".min.css";
-		}))
-		.pipe(dest("wwwroot/code-styles"));
-}
-
-function img() {
-	return src("Resources/CodeStyles/src/**/*.{png,jpg}")
-		.pipe(dest("wwwroot/code-styles"));
-}
-
 function jsMain() {
 	const mainJs = src("Resources/MudBlazor.Markdown.js")
 		.pipe(webpack({ mode: "production" }))
@@ -44,4 +28,4 @@ function jsMain() {
 	return all(mainJs, mathJax);
 }
 
-exports.default = series(fonts, cssMain, cssCodeStyles, img, jsMain);
+exports.default = series(fonts, cssMain, jsMain);
